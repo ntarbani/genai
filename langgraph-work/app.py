5bdb6d9f554f299d0c20f37bf9071f740ebb351b
ntarbani <61533173+ntarbani@users.noreply.github.com>
2025-10-17 16:15:52 +0530

    Add files via upload

---
+++ langgraph-work/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+
+CONFIG = {'configurable':{'thread_id':"1"}}
+if 'message_history' not in st.session_state:
+  st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+  with st.chat_message(message['role']):
+      st.write(message['content'])
+
+user_input = st.chat_input("type here")
+
+if user_input:
+  st.session_state['message_history'].append({'role':'user','content':user_input})
+  with st.chat_message('user'):
+      st.write(user_input)
+  ai_message = chatbot.invoke({'messages':[HumanMessage(content =user_input )]},config=CONFIG)['messages'][-1].content
+  st.session_state['message_history'].append({'role':'assistant','content':ai_message})
+  with st.chat_message('assistant'):
+      st.write(ai_message)
+
